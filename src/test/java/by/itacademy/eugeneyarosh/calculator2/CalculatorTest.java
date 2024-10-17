90bd910d0e2f27fd998866fe1dda1c1e8def41c9
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-10-17 20:33:43 +0300

    refactor CalculatorTest, add sum1

--- src/test/java/by/itacademy/eugeneyarosh/calculator2/CalculatorTest.java
+++ src/test/java/by/itacademy/eugeneyarosh/calculator2/CalculatorTest.java
@@ -1,4 +1,15 @@
 package by.itacademy.eugeneyarosh.calculator2;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CalculatorTest {
+
+    @Test
+    public void sum1() {
+        Calculator calculator = new Calculator();
+
+        double actual = calculator.sum(1.3, 2.3);
+        Assertions.assertEquals(3.6, actual, 0.001);
+    }
 }
